bf2bc0892df9773a61a4fcd61f2e168727d29d05
Justin Wheeler <justinwwheeler@gmail.com>
2021-08-19 21:19:14 +0000

    adds index module, refactored fetcher with a function taking in a callback

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,25 +1,27 @@
 const request = require('request');
 
-const breedName = process.argv[2];
+const fetchBreedDescription = (breedName, callback) => {
+  const breedAPI = `https://api.thecatapi.com/v1/breeds/search/?q=${breedName}`;
+  
+  request(breedAPI, (error, response, body) => {
+    // Check for error with URL first
+    if (error) {
+      callback(error, null);
+      process.exit();
+    }
+    
+    // Parse the body to JSON format
+    const data = JSON.parse(body);
+    
+    // If the breed does not exist, the API will return an empty array so check for length 0
+    if (data.length === 0) {
+      callback('Breed name not found in database.', null);
+      process.exit();
+    }
+    
+    // All filters pass, log out cat description
+    callback(null, data[0].description);
+  });
+};
 
-const breedAPI = `https://api.thecatapi.com/v1/breeds/search/?q=${breedName}`;
-
-request(breedAPI, (error, response, body) => {
-  // Check for error with URL first
-  if (error) {
-    console.log(`Error with URL. ${error}`);
-    process.exit();
-  }
-  
-  // Parse the body to JSON format
-  const data = JSON.parse(body);
-  
-  // If the breed does not exist, the API will return an empty array so check for length 0
-  if (data.length === 0) {
-    console.log('Breed name not found in database.');
-    process.exit();
-  }
-  
-  // All filters pass, log out cat description
-  console.log(data[0].description);
-});
+module.exports = { fetchBreedDescription };
